130073742e35dfceb37fd93cd3a537c6c2b9ed81
Luaranet <luaranet08@gmail.com>
2023-06-16 19:34:33 -0400

    comit inicial

---
+++ score.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('./conn.js');
+
+const router = express.Router();
+
+// Registrar um novo resultado
+router.post("/", async (req, res) => {
+    let col = await db.collection('score');
+    let resul = col.insertOne(req.body);
+    res.send(out).status(204);
+    // seu código aqui incert
+});
+
+
+// Pegar os 10 melhores resultados
+router.get("/", async (req, res) => {
+    let col = await db.historico.find('score');
+    let out = await col.find().sort({
+        pontos: -1
+        }).limit(10).toArray();
+        res.send(out).status(200);
+    // seu código aqui fait
+});
+
+module.exports = router;
